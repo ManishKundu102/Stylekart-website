88e651329ec77a08eed1009e6d958943c3c1300a
ManishKundu102 <kundumanish50@gmail.com>
2025-08-03 16:41:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </Router>
     </CartProvider>
